e7355e7047300439df2811edc599179dc6fef81c
VictorApaez <vic.apaez@gmail.com>
2023-08-11 16:34:32 -0400

    created component to display recipe data

--- components/RecipePage.tsx
+++ components/RecipePage.tsx
@@ -1,62 +1,12 @@
 import React from 'react';
 import {View, Text, Image, ScrollView, StyleSheet} from 'react-native';
-import {Recipe} from './CategoriesData';
 
-const RecipePage: React.FC<any> = ({route}) => {
-  const {recipes, categoryType} = route.params;
-  console.log(categoryType);
+export const RecipePage: React.FC<any> = ({route}) => {
+  const {recipe} = route.params;
+
   return (
     <ScrollView>
-      <Text style={styles.categoryType}>{categoryType}</Text>
-      {recipes.map((recipe: Recipe, key: number) => (
-        <View key={key} style={styles.recipeContainer}>
-          <View style={styles.imageContainer}>
-            <Image source={recipe.image} style={styles.recipeImage} />
-            <View style={styles.overlay} />
-            <Text style={styles.recipeName}>{recipe.name}</Text>
-          </View>
-        </View>
-      ))}
+      <Text>{recipe.name}</Text>
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  categoryType: {
-    fontSize: 30,
-    color: 'black',
-    textAlign: 'center',
-  },
-  recipeContainer: {
-    padding: 1,
-  },
-  imageContainer: {
-    position: 'relative',
-    width: '100%',
-    height: 200,
-  },
-  recipeImage: {
-    width: '100%',
-    height: '100%',
-  },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'black',
-    opacity: 0.5,
-  },
-  recipeName: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    color: 'white',
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    fontSize: 24,
-  },
-});
-
-export default RecipePage;
