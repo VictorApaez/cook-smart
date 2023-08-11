ce230e7139bb0339f1f981378d2082bffb11e6f8
VictorApaez <vic.apaez@gmail.com>
2023-08-11 15:09:32 -0400

    updated based on the new categories data format

--- components/CategoriesScreen.tsx
+++ components/CategoriesScreen.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import {View, Text, FlatList, Dimensions, StyleSheet} from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import CarouselItem from './CarouselItem';
-import {categories} from './CategoriesData';
-import {Category, SubCategory} from './CategoriesData';
+import {categories, CategoriesData, SubCategory} from './CategoriesData';
 
 const {width: screenWidth} = Dimensions.get('window');
 
 const CategoriesScreen: React.FC = () => {
-  const renderItem = ({item}: {item: Category}) => (
+  const renderSubCategories = (subCategory: SubCategory[], title: string) => (
     <View style={styles.categoryContainer}>
-      <Text style={styles.categoryTitle}>{item.title}</Text>
+      <Text style={styles.categoryTitle}>{title}</Text>
       <Carousel
-        data={item.data}
+        data={subCategory}
         renderItem={({item}) => <CarouselItem item={item} />}
         sliderWidth={screenWidth}
         itemWidth={160}
@@ -24,7 +23,7 @@ const CategoriesScreen: React.FC = () => {
   return (
     <FlatList
       data={categories}
-      renderItem={renderItem}
+      renderItem={({item}) => renderSubCategories(item.subCategory, item.title)}
       keyExtractor={(item, index) => index.toString()}
     />
   );
