2cbe758c541ae36978ff5ce75cd5e91368832f84
VictorApaez <vic.apaez@gmail.com>
2023-08-11 16:35:57 -0400

    added navigation to RecipePage

--- components/AppNavigator.tsx
+++ components/AppNavigator.tsx
@@ -1,6 +1,7 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import RecipePage from './RecipePage';
+import RecipesPage from './RecipesPage';
 import CategoriesScreen from './CategoriesScreen';
+import {RecipePage} from './RecipePage';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ function AppNavigator() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={CategoriesScreen} />
+      <Stack.Screen name="Recipes" component={RecipesPage} />
       <Stack.Screen name="Recipe" component={RecipePage} />
     </Stack.Navigator>
   );
