89fa427df36b043a8d4d73a06988f3f58ad85ec0
VictorApaez <vic.apaez@gmail.com>
2023-08-15 15:11:50 -0400

    created home stack for recipe navigation and added tab navigator with home button

--- components/AppNavigator.tsx
+++ components/AppNavigator.tsx
@@ -1,17 +1,30 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import RecipesPage from './RecipesPage';
 import CategoriesScreen from './CategoriesScreen';
 import {RecipePage} from './RecipePage';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-function AppNavigator() {
+function HomeStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={CategoriesScreen} />
       <Stack.Screen name="Recipes" component={RecipesPage} />
       <Stack.Screen name="Recipe" component={RecipePage} />
     </Stack.Navigator>
+  );
+}
+
+function AppNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Tab.Screen name="Home" component={HomeStack} />
+    </Tab.Navigator>
   );
 }
 
