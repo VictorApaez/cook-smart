f10f93f28aa01e478a2ea13ca1bb1cf89a924001
VictorApaez <vic.apaez@gmail.com>
2023-08-15 15:09:59 -0400

    removed unused import

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './components/AppNavigator';
-import CategoriesScreen from './components/CategoriesScreen';
 
 const App: React.FC = () => {
   return (
