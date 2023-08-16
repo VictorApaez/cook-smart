01e95175e2b0bbb93ba6f5fe832bb3716e367d3a
VictorApaez <vic.apaez@gmail.com>
2023-08-16 15:28:59 -0400

    created component page for displaying recipe form

---
+++ components/CreateRecipePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, FlatList, Dimensions, StyleSheet} from 'react-native';
+
+export const CreateRecipePage: React.FC = () => {
+  return (
+    <View>
+      <Text>Create Recipe!</Text>
+    </View>
+  );
+};
