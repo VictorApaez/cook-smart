2c9d37547a767635232f5b865cf3735eaf6ba9e8
VictorApaez <vic.apaez@gmail.com>
2023-08-17 15:15:04 -0400

    added backdrop to Modal and wrapperd it in TouchableWithoutFeedback to clode modal when backdrop is touched

--- components/TagEditor.tsx
+++ components/TagEditor.tsx
@@ -7,6 +7,7 @@ import {
   FlatList,
   Modal,
   StyleSheet,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import {recipeTags} from './CategoriesData';
 
@@ -45,52 +46,71 @@ export const TagEditor: React.FC<TagEditorProps> = ({
   };
 
   return (
-    <Modal visible={visible} onRequestClose={onClose}>
-      <View style={styles.modalContainer}>
-        <View style={styles.selectedTagsContainer}>
-          {selectedTags.map((tag: string, index: number) => (
-            <TouchableOpacity
-              key={index}
-              onPress={() => handleTagRemove(tag)}
-              style={styles.tagButton}>
-              <Text style={styles.tagButtonText}>{tag}</Text>
-              <Text style={styles.tagButtonRemoveText}>×</Text>
-            </TouchableOpacity>
-          ))}
+    <Modal
+      visible={visible}
+      onRequestClose={onClose}
+      transparent={true}
+      animationType="fade">
+      <TouchableWithoutFeedback onPress={onClose}>
+        <View style={styles.modalBackdrop}>
+          <TouchableWithoutFeedback>
+            <View style={styles.modalContainer}>
+              <View style={styles.selectedTagsContainer}>
+                {selectedTags.map((tag: string, index: number) => (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() => handleTagRemove(tag)}
+                    style={styles.tagButton}>
+                    <Text style={styles.tagButtonText}>{tag}</Text>
+                    <Text style={styles.tagButtonRemoveText}>×</Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+
+              <TextInput
+                value={searchQuery}
+                onChangeText={setSearchQuery}
+                placeholder="Search tags..."
+                style={styles.searchInput}
+              />
+
+              <FlatList
+                data={availableTags}
+                keyExtractor={item => item}
+                renderItem={({item}) => (
+                  <TouchableOpacity
+                    onPress={() => handleTagPress(item)}
+                    style={styles.tagItem}>
+                    <Text style={styles.tagItemText}>{item}</Text>
+                    <Text style={styles.tagItemText}>+</Text>
+                  </TouchableOpacity>
+                )}
+              />
+
+              <TouchableOpacity onPress={handleSave} style={styles.saveButton}>
+                <Text style={styles.saveButtonText}>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableWithoutFeedback>
         </View>
-
-        <TextInput
-          value={searchQuery}
-          onChangeText={setSearchQuery}
-          placeholder="Search tags..."
-          style={styles.searchInput}
-        />
-
-        <FlatList
-          data={availableTags}
-          keyExtractor={item => item}
-          renderItem={({item}) => (
-            <TouchableOpacity
-              onPress={() => handleTagPress(item)}
-              style={styles.tagItem}>
-              <Text style={styles.tagItemText}>{item}</Text>
-              <Text style={styles.tagItemText}>+</Text>
-            </TouchableOpacity>
-          )}
-        />
-
-        <TouchableOpacity onPress={handleSave} style={styles.saveButton}>
-          <Text style={styles.saveButtonText}>Save</Text>
-        </TouchableOpacity>
-      </View>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
+  modalBackdrop: {
+    flex: 1,
+    justifyContent: 'center', // Center the modal vertically
+    alignItems: 'center', // Center the modal horizontally
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
+  },
   modalContainer: {
+    width: '90%', // Take up 90% of screen width
+    maxHeight: '80%', // Take up max of 80% of screen height
     padding: 20,
     backgroundColor: '#FFFFFF',
+    borderRadius: 10,
     flex: 1,
   },
   searchInput: {
