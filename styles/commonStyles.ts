05324a5571616453d92c0630797db9fe6493cc74
VictorApaez <vic.apaez@gmail.com>
2023-08-17 15:46:28 -0400

    added a styles folder for sharing common styles

---
+++ styles/commonStyles.ts
@@ -0,0 +1,35 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#f5f5f5',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 8,
+  },
+});
+
+export const modalStyles = StyleSheet.create({
+  modalBackdrop: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    width: '90%',
+    maxHeight: '80%',
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    flex: 1,
+  },
+});
