9c40e3c945722991d7b2a20b840f0d83f98ba05a
VictorApaez <vic.apaez@gmail.com>
2023-09-07 13:47:59 -0400

    created test for categories thunk: should update state when thunk is successful

---
+++ __tests__/categoriesSlice.test.js
@@ -0,0 +1,34 @@
+import {configureStore} from '@reduxjs/toolkit';
+import axios from 'axios';
+import categoriesReducer from '../redux/categories/categoriesSlice';
+import {fetchCategories} from '../redux/categories/categoriesThunks';
+import recipesReducer from '../redux/recipes/recipesSlice';
+import {categories} from '../components/CategoriesData';
+
+jest.mock('axios');
+
+describe('categories thunk tests', () => {
+  let store;
+
+  beforeEach(() => {
+    store = configureStore({
+      reducer: {
+        recipes: recipesReducer,
+        categories: categoriesReducer,
+      },
+    });
+  });
+
+  it('should update categories state on thunk success', async () => {
+    axios.get.mockResolvedValueOnce({
+      data: categories,
+    });
+
+    await store.dispatch(fetchCategories());
+
+    const currentState = store.getState();
+
+    expect(currentState.categories.status).toBe('succeeded');
+    expect(currentState.categories.categories).toEqual(categories);
+  });
+});
