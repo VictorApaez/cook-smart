fea769c340be4087017b18a2f90e2ff6c3434e1b
VictorApaez <vic.apaez@gmail.com>
2023-09-07 14:51:06 -0400

    added types to createAsyncThunk

--- redux/categories/categoriesThunks.ts
+++ redux/categories/categoriesThunks.ts
@@ -1,15 +1,17 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {categories} from '../../components/CategoriesData';
+import {Category} from '../../components/CategoriesData';
 
-export const fetchCategories = createAsyncThunk(
-  'categories/fetchCategories',
-  async () => {
-    // simulating data loading for 3 seconds
-    const response = await new Promise(resolve => {
-      setTimeout(() => {
-        resolve(categories);
-      }, 3000);
-    });
-    return response;
-  },
-);
+export const fetchCategories = createAsyncThunk<
+  // return type
+  Category[],
+  // argument type
+  void,
+  // config object
+  {}
+>('categories/fetchCategories', async () => {
+  //simulate db fetch
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(categories), Math.random() * 1000);
+  });
+});
