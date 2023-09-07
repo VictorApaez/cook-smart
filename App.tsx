d835a7a13e6e067c0dbc6d420c48745a7a3cdf03
VictorApaez <vic.apaez@gmail.com>
2023-09-07 14:23:29 -0400

    wrapped app with redux provider

--- App.tsx
+++ App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './components/AppNavigator';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        <AppNavigator />
-      </SafeAreaView>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaView style={styles.container}>
+          <AppNavigator />
+        </SafeAreaView>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
