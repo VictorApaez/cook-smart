654ab2f3bf8d1ccf6078e1e7e354c699d7645a46
VictorApaez <vic.apaez@gmail.com>
2023-09-07 13:41:27 -0400

    created thunk to handle async updates to db and state

---
+++ redux/recipes/recipesThunks.ts
@@ -0,0 +1,13 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import {recipes} from '../../components/CategoriesData';
+
+export const fetchRecipes = createAsyncThunk(
+  'recipes/fetchRecipes',
+  async () => {
+    //simulating fetching data
+    // const response = await fetch('YOUR_DB_ENDPOINT_HERE');
+    const response = recipes;
+
+    return response;
+  },
+);
