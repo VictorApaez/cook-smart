b0f9d2d304ef5a5c5007f76794cd7bf5f453ed3c
VictorApaez <vic.apaez@gmail.com>
2023-09-07 13:40:31 -0400

    created slice with empty inital state and reducers to add/remove category and extra reducers to update status of thunks

---
+++ redux/categories/categoriesSlice.ts
@@ -0,0 +1,45 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {fetchCategories} from './categoriesThunks';
+import {Category} from '../../components/CategoriesData';
+
+export type CategoriesState = {
+  categories: Category[];
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+};
+
+const initialState: CategoriesState = {
+  categories: [],
+  status: 'idle',
+};
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    addCategory: (state, action) => {
+      state.categories.push(action.payload);
+    },
+    removeCategory: (state, action) => {
+      state.categories = state.categories.filter(
+        category => category.title !== action.payload,
+      );
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.categories = action.payload;
+      })
+      .addCase(fetchCategories.rejected, (state, action) => {
+        state.status = 'failed';
+      })
+      .addCase(fetchCategories.pending, (state, action) => {
+        state.status = 'loading';
+      });
+  },
+});
+
+export const {addCategory, removeCategory} = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
