665be42362716d5e8127d8ce760e16cfc4ac3d86
VictorApaez <vic.apaez@gmail.com>
2023-09-08 16:28:50 -0400

    added close icon

--- components/ContentModal.tsx
+++ components/ContentModal.tsx
@@ -1,5 +1,6 @@
 import {Modal, TouchableWithoutFeedback, View} from 'react-native';
 import {modalStyles} from '../styles/commonStyles';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome.js';
 
 type ContentModalTypes = {
   onClose: () => void;
@@ -21,7 +22,22 @@ export const ContentModal: React.FC<ContentModalTypes> = ({
       <TouchableWithoutFeedback onPress={onClose}>
         <View style={modalStyles.modalBackdrop}>
           <TouchableWithoutFeedback>
-            <View style={modalStyles.modalContainer}>{children}</View>
+            <View style={modalStyles.modalContainer}>
+              <FontAwesomeIcon
+                name="times-circle"
+                size={30}
+                color="#000"
+                style={{
+                  position: 'absolute',
+                  top: 10,
+                  right: 10,
+                  color: '#ED2939',
+                  zIndex: 10,
+                }}
+                onPress={onClose}
+              />
+              {children}
+            </View>
           </TouchableWithoutFeedback>
         </View>
       </TouchableWithoutFeedback>
