fef3efe45d00604e245afff8a5434301c48013fa
VictorApaez <vic.apaez@gmail.com>
2023-09-11 13:57:46 -0400

    changed file name

---
+++ components/RecipesScreen.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import {Text, ScrollView, StyleSheet} from 'react-native';
+import {Recipe} from '../../data/CategoriesData';
+import RecipesItem from './RecipesItem';
+
+interface RecipesPageProps {
+  route?: any;
+  recipes?: Recipe[];
+  categoryType?: string;
+}
+
+const RecipesPage: React.FC<RecipesPageProps> = ({
+  route,
+  recipes: propRecipes,
+  categoryType: propCategoryType,
+}) => {
+  // This component will accept recipes from either props or from the route object (navigation)
+  const {recipes: routeRecipes, categoryType: routeCategoryType} =
+    route?.params || {};
+
+  const recipes = propRecipes || routeRecipes;
+  const categoryType = propCategoryType || routeCategoryType;
+
+  return (
+    <ScrollView>
+      <Text style={styles.categoryType}>{categoryType}</Text>
+      {recipes.map((recipe: Recipe, key: number) => (
+        <RecipesItem recipe={recipe} id={key} />
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  categoryType: {
+    fontSize: 30,
+    color: 'black',
+    textAlign: 'center',
+  },
+});
+
+export default RecipesPage;
