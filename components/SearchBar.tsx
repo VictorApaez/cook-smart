ea5994414b9b41ded976ecf4e95c9853bfa1dafa
VictorApaez <vic.apaez@gmail.com>
2023-09-19 14:17:36 -0400

    on change the searchbar will filter data based on categories and recipes

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -10,15 +10,21 @@ import {
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import {iconStyles} from '../styles/commonStyles';
-
 import {SearchBarProps} from './AppNavigator';
+import {useTypedNavigation} from '../hooks/useTypedNavigation';
+import {Recipe} from '../data/CategoriesData';
 
 export const SearchBar: React.FC<SearchBarProps> = ({route}) => {
-  const [text, setText] = useState('');
-  const originalList = useMemo(() => route.params.list, [route.params.list]);
-  const [list, setList] = useState(route.params.list);
+  const originalList = route.params.list;
 
-  const handleItemPress = (selectedItem: {name: string}) => {
+  const [text, setText] = useState('');
+  const [list, setList] = useState(originalList);
+  const navigation = useTypedNavigation();
+
+  const handleItemPress = (selectedItem: any, type: string) => {
+    if (type === 'recipe') {
+      navigation.navigate('RecipeDetail', {recipe: selectedItem});
+    }
     setText('');
   };
 
@@ -28,9 +34,23 @@ export const SearchBar: React.FC<SearchBarProps> = ({route}) => {
     if (text === '') {
       setList(originalList);
     } else {
-      const filteredList = originalList.filter(item =>
-        item.name.toLowerCase().includes(text.toLowerCase()),
-      );
+      const filteredList = originalList
+        .map(category => {
+          const filteredRecipes = category.recipes.filter(recipe =>
+            recipe.name.toLowerCase().includes(text.toLowerCase()),
+          );
+
+          return {
+            ...category,
+            recipes: filteredRecipes,
+          };
+        })
+        .filter(
+          category =>
+            category.recipes.length > 0 ||
+            category.name.toLowerCase().includes(text.toLowerCase()),
+        );
+
       setList(filteredList);
     }
   };
@@ -46,14 +66,34 @@ export const SearchBar: React.FC<SearchBarProps> = ({route}) => {
       <View style={[styles.dropdown, {zIndex: 1}]}>
         {list.map((listItem: any, index: number) => (
           <TouchableWithoutFeedback
-            onPress={() => handleItemPress(listItem)}
+            onPress={() => handleItemPress(listItem, 'category')}
             key={listItem.name + index}>
             <View style={styles.dropdownItemContainer}>
-              <View style={styles.dropdownItemLeft}>
-                <FontAwesomeIcon name="search" style={iconStyles.default} />
-                <Text style={styles.dropdownItemText}>{listItem.name}</Text>
+              <View style={styles.dropdownItemCategory}>
+                <View style={styles.dropdownCategoryLeft}>
+                  <FontAwesomeIcon name="search" style={iconStyles.default} />
+                  <Text style={styles.dropdownItemText}>{listItem.name}</Text>
+                </View>
+                <FeatherIcon name="arrow-up-left" style={iconStyles.default} />
               </View>
-              <FeatherIcon name="arrow-up-left" style={iconStyles.default} />
+              {listItem.recipes && (
+                <View style={styles.dropDownItemRecipes}>
+                  {listItem.recipes.map((recipe: Recipe) => (
+                    <TouchableWithoutFeedback
+                      onPress={() => handleItemPress(recipe, 'recipe')}>
+                      <View style={styles.dropDownItemRecipeContainer}>
+                        <Text style={styles.dropDownItemRecipe}>
+                          {recipe.name}
+                        </Text>
+                        <FeatherIcon
+                          name="arrow-up-left"
+                          style={iconStyles.default}
+                        />
+                      </View>
+                    </TouchableWithoutFeedback>
+                  ))}
+                </View>
+              )}
             </View>
           </TouchableWithoutFeedback>
         ))}
@@ -83,19 +123,32 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   dropdownItemContainer: {
+    flexDirection: 'column',
+  },
+  dropdownItemCategory: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 10,
-    paddingLeft: 10,
-    paddingRight: 10,
     justifyContent: 'space-between',
   },
-  dropdownItemLeft: {
+  dropdownCategoryLeft: {
     flexDirection: 'row',
     alignItems: 'center',
   },
   dropdownItemText: {
     padding: 10,
     fontSize: 20,
+  },
+  dropDownItemRecipes: {
+    flexDirection: 'column',
+    marginLeft: 40,
+  },
+  dropDownItemRecipeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  dropDownItemRecipe: {
+    padding: 10,
+    fontSize: 15,
   },
 });
